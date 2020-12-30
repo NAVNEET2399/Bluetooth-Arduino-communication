3dc46ee0b6dd15c5f1e80861ccc5f9de16f6254e
Navneet Kumar <42838289+NAVNEET2399@users.noreply.github.com>
2020-12-30 20:36:25 +0530

    Create bluetooth_python.py

---
+++ examples/bluetooth_python.py
@@ -0,0 +1,20 @@
+import serial
+import time
+print("Starting Communication between Bluetooth and python")
+port = "COM21"  # specify the com port  - check the COM Port in Device manager (Windows)
+while True:
+    try:
+        bluetooth = serial.Serial(port, 9600)    // create instance of Serial communication
+        print("Connection Established")
+        break
+    except:
+        print("Waiting for Connection")
+        pass
+        
+def readData():
+    raw_data = bluetooth.readline()
+    data = input_data.decode()
+    print(data)
+    time.sleep(1)
+
+readData()
